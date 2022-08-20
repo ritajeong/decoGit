fdb05c65c202b2c44ae0d994560a7bcb0e58fc3e
ritajeong <ritajeong.dev@gmail.com>
2022-08-20 16:43:44 +0900

    Refactor global css code about example sticker in main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,10 +16,10 @@ const Home: NextPage = () => {
         <section className="z-0 flex flex-col items-center w-full h-screen pt-40">
           <div className="z-50 laptop"></div>
           {login && <p className="z-50 pt-10 text-5xl font-black text-black uppercase">login with github</p>}
-          <div className="blured-icon"></div>
-          <div className="w-[120px] h-[120px] sticked-icon bg-[url('/assets/sticker/kotlin.svg')] left-40 top-60 blur-sm"></div>
-          <div className="w-[215px] h-[215px] sticked-icon bg-[url('/assets/sticker/c.svg')] right-40 top-60 blur-sm rotate-[17deg]"></div>
-          <div className="w-[300px] h-[215px] sticked-icon bg-[url('/assets/sticker/mysql.svg')] right-60 bottom-20 blur-sm"></div>
+          <div className="w-[971px] h-[247px] sticker bg-[url('/assets/logo@3x.png')] left-0 bottom-0 blur-md rotate-[13deg]"></div>
+          <div className="w-[120px] h-[120px] sticker bg-[url('/assets/sticker/kotlin.svg')] left-40 top-60 blur-sm"></div>
+          <div className="w-[215px] h-[215px] sticker bg-[url('/assets/sticker/c.svg')] right-40 top-60 blur-sm rotate-[17deg]"></div>
+          <div className="w-[300px] h-[215px] sticker bg-[url('/assets/sticker/mysql.svg')] right-60 bottom-20 blur-sm"></div>
         </section>
       </Layout>
     </>
