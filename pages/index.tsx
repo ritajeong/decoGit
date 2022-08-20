97a7f5a03ece796148979bad74eabda50cce0f4c
ritajeong <ritajeong.dev@gmail.com>
2022-08-20 15:46:43 +0900

    Add example sticker to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import { Layout } from "../components/layout";
-import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +10,8 @@ const Home: NextPage = () => {
           <p className="z-50 pt-10 text-5xl font-black text-black uppercase">login with github</p>
           <div className="blured-icon"></div>
           <div className="w-[120px] h-[120px] sticked-icon bg-[url('/assets/sticker/kotlin.svg')] left-40 top-60 blur-sm"></div>
+          <div className="w-[215px] h-[215px] sticked-icon bg-[url('/assets/sticker/c.svg')] right-40 top-60 blur-sm rotate-[17deg]"></div>
+          <div className="w-[300px] h-[215px] sticked-icon bg-[url('/assets/sticker/mysql.svg')] right-60 bottom-20 blur-sm"></div>
         </section>
       </Layout>
     </>
