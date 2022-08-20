1c336240042627d675edd4cd0e208b622ad78fcf
ritajeong <ritajeong.dev@gmail.com>
2022-08-21 00:54:37 +0900

    Remove wallet icon in navigation bar when login state

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -40,7 +40,7 @@ export function Navigation({ login, handleSignout, connectWallet }: Props) {
           </Link>
         </div>
         <div className="flex gap-4">
-          <Image onClick={connectWallet} src="/assets/icon-wallet.svg" alt="" width={36} height={36} />
+          {!login && <Image onClick={connectWallet} src="/assets/icon-wallet.svg" alt="" width={36} height={36} />}
           {login && (
             <>
               <Image src="/assets/icon-cart.svg" alt="" width={36} height={36} />
