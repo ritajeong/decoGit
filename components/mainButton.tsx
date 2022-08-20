722781361b30dbffd1e41edca1524bb3e798fff0
havana723 <71638185+havana723@users.noreply.github.com>
2022-08-20 19:59:38 +0900

    feat: button hover

--- components/mainButton.tsx
+++ components/mainButton.tsx
@@ -7,8 +7,9 @@ interface Props {
 export const MainButton: React.FC<PropsWithChildren<Props>> = (props) => {
   return (
     <>
-      <button className="border-4 border-[#ffffff] hover:bg-white p-4" onClick={props.onClick}>
-        {props.children}
+      <button className="border-4 border-[#ffffff] p-4 relative group" onClick={props.onClick}>
+        <div className="absolute w-full h-full bg-white left-0 top-0 scale-x-0 group-hover:scale-x-100 transition-all origin-left"></div>
+        <div className="relative">{props.children}</div>
       </button>
     </>
   );
