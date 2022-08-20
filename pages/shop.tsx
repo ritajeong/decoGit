64e3e262536f37ded3c280acb23fad4196c50814
havana723 <71638185+havana723@users.noreply.github.com>
2022-08-21 05:35:17 +0900

    feat: fetch data

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,9 +1,26 @@
+import axios from "axios";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Navigation } from "../components/navigation";
-import { stickers } from "../components/sticker/stickers";
+import { stickers as localStickers } from "../components/sticker/stickers";
 import { useInfo } from "../lib/InfoContext";
+
+interface StickerResponse {
+  index: string;
+  name: string;
+  price: string;
+  owner: string;
+}
+
+interface Response {
+  sticker: StickerResponse[];
+}
+
+function parseDeco(s: string): number {
+  s = s.substring(0, s.length - 5);
+  return +s;
+}
 
 const Shop: NextPage = () => {
   const { login, loading, keplr, github, handleGithub, handleSignout, connectWallet } = useInfo();
@@ -16,6 +33,19 @@ const Shop: NextPage = () => {
     }
   }, [login, loading]);
 
+  const [stickers, setStickers] = useState<Response | null>(null);
+
+  useEffect(() => {
+    axios
+      .get<Response>("http://5.server.susuyo.ai:1317/decogit/decogit/sticker", {})
+      .then((res) => {
+        setStickers(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
       <Navigation login={login} handleSignout={handleSignout} connectWallet={connectWallet} />
@@ -25,19 +55,29 @@ const Shop: NextPage = () => {
           <div className="flex justify-center">
             <div className="bg-white bg-cover w-full max-w-[840px] h-[70vh] align-center absolute bottom-0 overflow-hidden p-4">
               <div className="flex flex-wrap justify-center h-full gap-4 overflow-y-scroll align-center">
-                {Object.entries(stickers).map(([key, sticker]) => (
-                  <div className="w-[160px] p-2 shrink-0 mt-4 hover:bg-[#fff1f8] flex flex-col items-center" key={key}>
-                    <div
-                      className="h-[128px] w-[128px] bg-contain bg-no-repeat bg-center"
-                      style={{ backgroundImage: `url('/assets/${sticker.url}')` }}
-                    />
-
-                    <div className="flex justify-center">
-                      <span className="bg-[url('/assets/github/planet.svg')] bg-center bg-cover w-6 h-6"></span>
-                      <span className="pl-2">1024</span>
-                    </div>
-                  </div>
-                ))}
+                {stickers
+                  ? stickers.sticker
+                      .filter(({ owner }) => owner === "")
+                      .map(({ index, name, price, owner }) => (
+                        <div
+                          className="w-[160px] p-2 shrink-0 mt-4 hover:bg-[#fff1f8] flex flex-col items-center"
+                          key={index}
+                        >
+                          <div
+                            className="h-[128px] w-[128px] bg-contain bg-no-repeat bg-center"
+                            style={{
+                              backgroundImage: `url('/assets/${
+                                localStickers[name as keyof typeof localStickers].url
+                              }')`,
+                            }}
+                          />
+                          <div className="flex justify-center">
+                            <span className="bg-[url('/assets/github/planet.svg')] bg-center bg-cover w-6 h-6"></span>
+                            <span className="pl-2">{parseDeco(price) / 1000000}</span>
+                          </div>
+                        </div>
+                      ))
+                  : null}
               </div>
             </div>
           </div>
