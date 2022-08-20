3dcb140ed2fcde6ddd9a8895dd8a6ba9f863a47d
havana723 <71638185+havana723@users.noreply.github.com>
2022-08-21 03:07:52 +0900

    feat: shop box

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { Navigation } from "../components/navigation";
+import { stickers } from "../components/sticker/stickers";
 import { useInfo } from "../lib/InfoContext";
 
 const Shop: NextPage = () => {
@@ -18,7 +19,24 @@ const Shop: NextPage = () => {
   return (
     <>
       <Navigation login={login} handleSignout={handleSignout} connectWallet={connectWallet} />
-      <main className="bg-[url('/assets/bg-image.png')] bg-center bg-cover"></main>
+      <main className="bg-[url('/assets/bg-image.png')] bg-center bg-cover">
+        <section className="z-0 flex flex-col items-center w-full h-screen pt-32">
+          <p className="text-5xl font-black text-black uppercase">shop</p>
+          <div className="flex justify-center">
+            <div className="bg-white bg-cover w-[840px] h-[70vh] align-center absolute bottom-0 overflow-hidden p-4">
+              <div className="flex flex-wrap align-center h-full gap-4">
+                {Object.entries(stickers).map(([key, sticker]) => (
+                  <div
+                    className="h-[15vh] w-[15vh] bg-contain bg-no-repeat bg-center shrink-0 mt-4"
+                    key={key}
+                    style={{ backgroundImage: `url('/assets/${sticker.url}')` }}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
