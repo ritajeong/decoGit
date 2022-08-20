6fc437d84bd0febd7bbe3c32f17cb6b4c4382835
ritajeong <ritajeong.dev@gmail.com>
2022-08-20 11:17:39 +0900

    Modify meta tag in html head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>decoGit</title>
+        <meta name="author" content="decoGit" />
+        <meta name="description" content="Decorate your git profile" />
+        {/* <link rel="icon" href="/assets/head-icon.svg" /> */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
