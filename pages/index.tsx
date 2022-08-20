2941e03c5fd35d65821aa493a4feddc1b900402f
ritajeong <ritajeong.dev@gmail.com>
2022-08-20 15:34:28 +0900

    Add example sticker to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from "next";
 import { Layout } from "../components/layout";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <>
       <Layout>
-        <section className="pt-8 pl-4 h-[1020px] w-full sm:pl-10 md:pl-20 lg:pl-40 xl:pl-80">
-          <div className="labtop"></div>
+        <section className="z-0 flex flex-col items-center w-full h-screen pt-40">
+          <div className="z-50 labtop"></div>
+          <p className="z-50 pt-10 text-5xl font-black text-black uppercase">login with github</p>
+          <div className="blured-icon"></div>
+          <div className="w-[120px] h-[120px] sticked-icon bg-[url('/assets/sticker/kotlin.svg')] left-40 top-60 blur-sm"></div>
         </section>
       </Layout>
     </>
