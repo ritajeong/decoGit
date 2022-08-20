9368bcf0668337b53adf15f80096d1950231b42d
ritajeong <ritajeong.dev@gmail.com>
2022-08-20 17:38:51 +0900

    Add sns sharing section

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,18 +24,32 @@ const Home: NextPage = () => {
   return (
     <>
       <Layout login={login} handleSignout={handleSignout} handleSignin={handleSignin}>
-        <section className="z-0 flex flex-col items-center w-full h-screen pt-40">
+        <section className="z-0 flex flex-col items-center w-full h-screen pt-32">
+          {/* center labtop image */}
           <div className="z-50 laptop"></div>
+
+          {/* state1 - after sign in, before connecting github */}
           {login && !github && (
-            <p className="z-50 pt-10 text-5xl font-black text-black uppercase" onClick={handleGithub}>
+            <p className="z-50 pt-16 text-5xl font-black text-black uppercase" onClick={handleGithub}>
               login with github
             </p>
           )}
+
+          {/* state2 - after connecting github */}
           {github && (
-            <p className="z-50 pt-10 text-5xl font-black text-black uppercase" onClick={handleGithub}>
-              decorate!
-            </p>
+            <>
+              <p className="z-50 pt-16 text-5xl font-black text-black uppercase" onClick={handleGithub}>
+                decorate!
+              </p>
+              <div className="z-50 flex justify-center h-40 gap-8 pt-16 w-80">
+                <span className="bg-[url('/assets/sns/instagram.svg')] icon-sns"></span>
+                <span className="bg-[url('/assets/sns/facebook.svg')] icon-sns"></span>
+                <span className="bg-[url('/assets/sns/twitter.svg')] icon-sns"></span>
+              </div>
+            </>
           )}
+
+          {/* absolute sticker zone! */}
           <div className="w-[971px] h-[247px] sticker bg-[url('/assets/logo@3x.png')] left-0 bottom-0 blur-md rotate-[13deg]"></div>
           <div className="w-[120px] h-[120px] sticker bg-[url('/assets/sticker/kotlin.svg')] left-40 top-60 blur-sm"></div>
           <div className="w-[215px] h-[215px] sticker bg-[url('/assets/sticker/c.svg')] right-40 top-60 blur-sm rotate-[17deg]"></div>
