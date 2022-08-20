0f03c08ac829edd22e71504a25abe36c2347f39b
ritajeong <ritajeong.dev@gmail.com>
2022-08-20 14:33:03 +0900

    Modify layout and main page

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
-// import Image from "next/image";
-import { useState, ReactNode } from "react";
+import Image from "next/image";
 import Link from "next/link";
+import { useState, ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
@@ -13,35 +13,27 @@ export function Layout({ children }: Props) {
     console.log("clicked signout");
     setLogin(false);
   };
-  const handleSignin = () => {
-    // localStorage.removeItem("token");
-    console.log("clicked signin");
-    setLogin(true);
-  };
 
   return (
     <>
-      <nav className="flex items-center justify-between p-4">
-        <h1 className="pl-10 text-lg font-normal">decoGit</h1>
-        {/* <Image src="/assets/main-logo.svg" alt="" width={77} height={40}></Image> */}
-        <div className="flex">
-          <ul className="flex gap-4 text-sm font-normal">
-            <li>Connet wallet</li>
-            <li>My page</li>
-            <li>Shop</li>
-            {login ? (
-              <li onClick={handleSignout}>
-                <Link href="/">Sign out</Link>
-              </li>
-            ) : (
-              <li onClick={handleSignin}>
-                <Link href="/">Sign in</Link>
-              </li>
-            )}
-          </ul>
+      <nav className="flex items-center justify-between p-4 bg-black">
+        <div>
+          <Image src="/assets/logo@3x.png" alt="" width={120} height={30}></Image>
+        </div>
+        <div className="flex gap-4">
+          <Image src="/assets/icon-wallet.svg" alt="" width={40} height={40}></Image>
+          {login && (
+            <>
+              <Image src="/assets/icon-cart.svg" alt="" width={40} height={40}></Image>
+              <Image src="/assets/icon-user.svg" alt="" width={40} height={40}></Image>
+              <Link href="/">
+                <Image onClick={handleSignout} src="/assets/icon-logout.svg" alt="" width={40} height={40}></Image>
+              </Link>
+            </>
+          )}
         </div>
       </nav>
-      <main>{children}</main>
+      <main className="bg-[url('/assets/bg-image.png')] bg-center bg-cover">{children}</main>
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from "next";
 import { Layout } from "../components/layout";
-// import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <>
       <Layout>
-        <section className="pt-8 pl-4 h-[1020px] w-full bg-[linear-gradient(242.94deg,#0068ff_0%,#99ffff_100%)] sm:pl-10 md:pl-20 lg:pl-40 xl:pl-80">
-          <h1 className="text-2xl pb-4 text-[#fff98a]">Decorate your laptop!</h1>
+        <section className="pt-8 pl-4 h-[1020px] w-full sm:pl-10 md:pl-20 lg:pl-40 xl:pl-80">
           <div className="shadow-searchbar rounded-lg absolute h-[500px] opacity-80 w-[343px] z-40 bg-lightgrey-2 flex pl-4 sm:w-[400px] md:w-[600px] lg:w-[800px]">
             <p className="pt-2 pl-2 text-grey-1 text-display2">mac image</p>
           </div>
